3a0a808c2f9ad1c6d8797c8c07695bca0a900985
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-05 19:25:52 -0500

    created contact page

---
+++ src/pages/contact.jsx
@@ -0,0 +1,10 @@
+export default function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p> You can reach us at </p>
+        </div>
+    );
+}
+
+                            
