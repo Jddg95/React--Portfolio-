5d48b5f839d3c44cdc8a89d6ed6f02f90fd28a40
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-05 19:25:12 -0500

    added browser router and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,45 @@
-import ReactDOM from 'react-dom/client';
-import {createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { App } from 'App.jsx';
-import '/indexx.css ';
+// import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./pages/about.jsx";
+import Contact from "./pages/contact.jsx";
+import Portfolio from "./pages/portfolio.jsx";
+import Error from "./pages/error.jsx";
+import Resume from "./pages/resume.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([{
-    path: '/',
+const router = createBrowserRouter([
+  {
+    path: "/",
     element: <App />,
-    childeren: [], 
-},
+    errorElement: <Error />,
+    children: [
+      {
+        imdex: true,
+        element: <About />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/resume",
+        element: <Resume />,
+      },
+    ],
+  },
 ]);
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <RouterProvider router={router} />
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
