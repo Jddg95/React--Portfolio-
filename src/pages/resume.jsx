04f3b6ead167d9e2b6e5193365e98ef0331b00dc
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-05 19:27:00 -0500

    created resume page

---
+++ src/pages/resume.jsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>
+                This is a simple example of a React application with routing.
+            </p>
+        </div>
+    );
+}
