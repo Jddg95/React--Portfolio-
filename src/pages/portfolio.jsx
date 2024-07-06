8bf781d26ae9f191c7fcfd17a166debf06c2c4e1
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-05 19:26:40 -0500

    created portfolio page

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,10 @@
+export default function Portfolio() {                       
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <p>
+                This is a simple example of a React application with routing.
+            </p>
+        </div>
+    );
+}
