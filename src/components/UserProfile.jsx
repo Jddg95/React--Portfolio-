f3dfbdc7295531e105e2d937c23df160d6fbdbac
Juan Garcia <140569072+Jddg95@users.noreply.github.com>
2024-07-28 01:22:20 -0500

    added description and styling to about page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UserProfile.css";
 import defaultPhoto from "../assets/images/1704292126044.jpg"; // Adjust the path as necessary
 
 function UserProfile() {
-  const photo = defaultPhoto;
-  const name = "Your Name";
-  const description = "Your Description";
+  const [photo, setPhoto] = useState(defaultPhoto);
+  const name = "Juan Garcia";
+  const description = `Hi there! My name is Juan, and I'm a budding web developer currently completing a rigorous coding bootcamp at the University of Texas in Austin. I have experience in SQL, NoSQL, Node.js, HTML, CSS, React, and more. I'm passionate about building user-friendly, responsive web applications. Growing up, I faced financial challenges, which fueled my determination to succeed through technology. I'm excited to bring my skills to a dynamic team and contribute to meaningful projects. Let's connect and create something amazing together!`;
 
   return (
     <div className="user-profile-container">
